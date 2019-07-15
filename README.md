6e98f3a50e5e17abb0d7acd59564b3750423e3d9
Mich Torres <anahi@familiatorres.mx>
2019-07-15 14:34:45 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+
+
+# Work Node js
+=============
+
+## Introduction
+This exercise has the objective of practice the node js usage in webpages. Even though is a very simple configuration, it uses modules imports and requires (imports and exports) and a clear link to html.
+
+## Installation instructions
+On the terminal
+
+ ```
+git clone --depth 1 https://github.com/MichJames/Work2Express
+ ```
+
+## Usage instructions
+On the terminal:
+
+```
+cd Work1node
+node index.js
+```
+Read the instructions of the page and have fun.
+
+## List of contributors
+The code was made using the guidance of the examples provided by **Sergie Kudinov** and the main producers, **Michelle & Emmanuel** .
---
+++ index.js
@@ -0,0 +1,42 @@
+express = require('express')
+app = express()
+metrics = require('./metrics.js')
+
+app.set('port', 1337)
+
+app.listen(
+  app.get('port'),
+  () => console.log(`server listening on ${app.get('port')}`)
+)
+
+app.set('views', __dirname + "/views")
+app.set('view engine', 'ejs');
+
+
+app.get('/',
+(req,res) => res.render('intro.ejs', {name: req.params.name}))
+
+
+app.get('/hello/Team7',
+(req,res) =>res.render('helloT7.ejs', {name: req.params.name}))
+
+
+app.get('/hello/:name',
+(req,res) => res.send("Hello " + req.params.name))
+
+app.get('/hello',
+(req,res) => res.render('hello.ejs', {name: req.params.name}))
+
+app.use(function(err, req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+  });
+
+path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/metrics', (req, res) => {
+  metrics.get((err, data) => {
+    if(err) throw err
+    res.status(200).json(data)
+  })
+})
